01a1b5b9dbbdfed952837d113ed1d0e9388ae457
Ogliss <randomdragon2@hotmail.com>
2021-08-28 10:54:33 +0100

    HAR racial restriction fix

---
+++ 1.3/Source/CM_Semi_Random_Research/Class1.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using AlienRace;
+using System.Text;
+using HarmonyLib;
+
+namespace CM_Semi_Random_Research
+{
+    public static class AlienRaceUtility
+    {
+        public static bool enabled_AlienRaces = ModsConfig.ActiveModsInLoadOrder.Any((ModMetaData m) => m.PackageIdPlayerFacing == "erdelf.HumanoidAlienRaces");
+
+        public static bool HasRace(ResearchProjectDef rpd)
+        {
+            if (enabled_AlienRaces) return Alien(rpd);
+            else return true;
+        }
+
+        static bool Alien(ResearchProjectDef rpd)
+        {
+            if (!AlienRace.RaceRestrictionSettings.researchRestrictionDict.ContainsKey(key: rpd))
+            {
+                return true;
+            }
+            HarmonyPatches.UpdateColonistRaces();
+            HashSet<ThingDef> colonistRaces = AccessTools.Field(typeof(AlienRace.HarmonyPatches), "colonistRaces").GetValue(null) as HashSet<ThingDef>;
+            return RaceRestrictionSettings.CanResearch(colonistRaces, rpd);
+        }
+
+    }
+}
